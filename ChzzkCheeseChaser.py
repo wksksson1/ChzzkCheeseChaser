5e9c74cf21a28ba54b1c37ec9c81785e85bfa2f5
Siwoo Choi <wksson1@gmail.com>
2025-04-13 01:55:09 +0900

    스트리머별 도네 총액과 전체 총액을 보여주는 기능을 추가했습니다.

--- ChzzkCheeseChaser.py
+++ ChzzkCheeseChaser.py
@@ -5,7 +5,7 @@ import os
 import argparse
 import sys
 
-class CheeseAccount:
+class purchaseList:
 
     def __init__(self, NID_AUT, NID_SES):
         self.__cookies = {'NID_AUT': NID_AUT, 'NID_SES': NID_SES}
@@ -53,23 +53,72 @@ class CheeseAccount:
         self.__purchaseList += newPurchaseList                          #기존 구매리스트에 신규 구매스리스트 추가
         self.__lastRefreshTime = time.time()                          #마지막 검색 시간을 갱신
 
-
     def getEntireList(self):
         return self.__purchaseList
         
 
     def getTotalPrice(self):
+        totalCheese = 0
         totalPrice = 0
         for purchase in self.__purchaseList:
             totalPrice += int(purchase["purchasePrice"])
-        return totalPrice
+            totalCheese += int(purchase["chargeAmount"])
+        return [totalCheese,totalPrice]
     
-    def exportAsFile(self,filePath=(os.getcwd() + "\\output.txt")):
-        file = open(filePath, 'w')
-        file.write(str(self.getEntireList()))
-        file.close()
 
+class usedList:
+    def __init__(self, NID_AUT, NID_SES):
+        self.__cookies = {'NID_AUT': NID_AUT, 'NID_SES': NID_SES}
+        self.__lastRefreshTime = 0     #검색 시간을 초기화
+        self.__purchaseList = []        # 구매 리스트를 초기화
+        self.refreshList()
     
+    def refreshList(self):
+        newPurchaseList = []
+
+        #헤더와 기본 Url
+        headers={"user-agent":"Mozilla/5.0 (compatible; exhentai-extractor;)", "referer":"https://game.naver.com/profile"}
+        currentTime = time.strftime("%Y %m").split(" ")
+        index = (int(currentTime[0]) - 2023)*12 + int(currentTime[1]) 
+
+        for yearMonth in range(0,index):
+            
+            monthlyList = []
+            requestURL = f"https://api.chzzk.naver.com/commercial/v1/product/purchase/history?page={0}&size=10&searchYear={yearMonth // 12 + 2023}&searchMonth={yearMonth % 12 + 1}"
+            apiResponse = requests.get(requestURL, headers=headers, cookies=self.__cookies)
+            if apiResponse.status_code != 200:
+                continue
+            apiJson = json.loads(apiResponse.content)
+            totalPageCount = apiJson["content"]["totalPages"]
+            print(f"{yearMonth // 12 + 2023}년 {yearMonth % 12 + 1:2}월 작업중...")
+            time.sleep(2)
+            for i in range (0, totalPageCount):                                                                  
+                requestURL = f"https://api.chzzk.naver.com/commercial/v1/product/purchase/history?page={i}&size=10&searchYear={yearMonth // 12 + 2023}&searchMonth={yearMonth % 12 + 1}"
+                apiResponse = requests.get(requestURL, headers=headers, cookies=self.__cookies)    
+                apiJson = json.loads(apiResponse.content)                                        # apiResponse를를 딕셔너리화
+                tempList = apiJson["content"]["data"]                                            # i번째 페이지의 치즈 리스트 
+                for j in range(0, len(tempList)):                                                # i번째 페이지의 구매별 데이터
+                    tempDict = dict(tempList[j])                                                 # 데이터를 dict화                
+                    monthlyList.append(tempDict)
+            if len(monthlyList) == 0:
+                continue
+            monthlyList.reverse()
+            newPurchaseList += monthlyList                                    
+        self.__purchaseList = newPurchaseList                          
+        self.__lastRefreshTime = time.time()
+
+    def getlistOfSent(self):
+        streamerTuple = {}
+        for lineData in self.__purchaseList:
+            if not(lineData["channelName"] in streamerTuple):
+                streamerTuple[lineData["channelName"]] = [lineData["payAmount"],1]
+                continue
+            streamerTuple[lineData["channelName"]][0] += lineData["payAmount"]
+            streamerTuple[lineData["channelName"]][1] += 1
+        return streamerTuple
+
+    def getEntireList(self):
+        return self.__purchaseList
 
 
 
@@ -79,11 +128,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--NID_AUT", type=str, nargs=1, help="NID_AUT 수동입력", required=False)
     parser.add_argument("--NID_SES", type=str, nargs=1, help="NID_SES 수동입력", required=False)
-    parser.add_argument("-f", "--file", nargs='?', default="", const="purchaseList.txt", type=str)
+    parser.add_argument("-f", "--file", action="store_true")
     parser.add_argument("-s", "--skip", action="store_true")
     args = parser.parse_args()
     os.system('cls')
-    print("ChzzkCheeseChaser는 치즈를 구매하기 위해 쓴 총액을 구하는 프로그램 입니다.")
+    print("ChzzkCheeseChaser는 치즈를 구매/사용용 총액을 구하는 프로그램 입니다.")
     print("치지직의 로그인이 필수이기 때문에 사용자의 쿠키가 필요합니다.")
     print("입력한 쿠키는 cookie.txt에만 저장되며 이 프로그램 실행시 자동로그인 외의 용도로 쓰이지 않습니다.")
     print("쿠키를 얻는 방법은 https://hajoung56.tistory.com/108 을 참고하세요\n")
@@ -136,9 +185,28 @@ def main():
     try:
         os.system("cls")
         print("작업중입니다...")
-        myAccount = CheeseAccount(NID_AUT=cookies["NID_AUT"], NID_SES=cookies["NID_SES"])
+        myAccount = purchaseList(NID_AUT=cookies["NID_AUT"], NID_SES=cookies["NID_SES"])
+        purchase = myAccount.getTotalPrice()
 
-        print("\n총 금액 : " + str(myAccount.getTotalPrice()) + "원\n")
+        myDonation = usedList(NID_AUT=cookies["NID_AUT"], NID_SES=cookies["NID_SES"])
+        donation = myDonation.getlistOfSent()
+        cheeseSum = 0
+
+        os.system('cls')
+        print("치즈 구매 종합")
+        print(f"총 구매 치즈 : {purchase[0]:10} 치즈")
+        print(f"총 구매 금액 : {purchase[1]:10} 원\n")
+
+        print("스트리머 도네 금액 리스트")
+        maxLength = 0
+        for name in donation.keys():
+            if maxLength < len(name):
+                maxLength = len(name)
+        for name in donation.keys():
+            print(f"{name + ' '*(maxLength-len(name))}\t: {donation[name][0]:9} 치즈 ({donation[name][1]:6} 회)")
+            cheeseSum += donation[name][0]
+        print(f"총 도네 치즈 \t: {cheeseSum:10} 치즈 ")
+        print(f"남은 치즈   \t: {purchase[0]-cheeseSum:10} 치즈 \n")
     except:
         print("문제가 발생했습니다.")
         print("쿠키의 문제일 수 있으니 cookie.txt를 지우고 다시 한 번 시도해보세요")
@@ -152,15 +220,21 @@ def main():
     except:
         print("쿠키 파일 작성 실패")
 
+
     finally:
         cookieFile.close()
 
-    if args.file != "" or fileStore:
+    if args.file or fileStore:
         try:
-            outputPath = basePath + (args.file if args.file != "" else 'purchaseList.txt')
+            outputPath = basePath + 'purchaseList.txt'
             outputFile = open(outputPath, 'w', encoding="utf-8")
             outputFile.write('"{ list" : ' + str(myAccount.getEntireList()).replace('},','},\n ').replace('[', '[\n  ') + "}")
             outputFile.close()
+
+            donationPath = basePath + 'donationList.txt'
+            donationFile = open(donationPath, 'w', encoding="utf-8")
+            donationFile.write('"{ list" : ' + str(myDonation.getEntireList()).replace('},','},\n ').replace('[', '[\n  ') + "}")
+            donationFile.close()
             print("목록 파일 저장에 성공했습니다.")
         except:
             print("목록 파일 작성 실패")
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Chzzk Cheese Chaser
 
-치즈를 구매하기 위해 사용한 총 금액을 알려줍니다.  
+치즈의 행방을 찾기 위한 프로그램입니다.  
 
 ### 릴리즈
 https://github.com/wksksson1/ChzzkCheeseChaser/releases/
@@ -13,5 +13,4 @@ NID_AUT, NID_SES 2개의 쿠키가 필요합니다.
 >  1. 릴리즈에서 파일을 다운로드 합니다.
 >  2. 파일의 압축을 풀고 ChzzkCheeseChaser.exe를 실행합니다.
 >  3. 프롬프트에서 요구하는 대로 NID_AUT, NID_SES를 입력합니다.
->  4. 총 금액이 나옵니다.
-
+>  4. 구매한 치즈들과 사용한 치즈들의 총합을 알려줍니다. 
